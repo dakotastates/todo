1992778e364e70918ce2922d40a736aec33457d8
dakotastates <59000108+dakotastates@users.noreply.github.com>
2023-02-02 12:42:12 -0800

    speech to text client
    Created a speech to text function which creates notes from speech.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./index.css";
 
-function App() {
+function App() { 
+
+  const [isRecording, setisRecording] = useState(false);
+  const [note, setNote] = useState(null);
+  const [notesStore, setnotesStore] = useState([]); 
+
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const microphone = new SpeechRecognition();
+
+  microphone.continuous = false;
+  microphone.interimResults = true;
+  microphone.lang = "en-US"; 
+
+  const startRecordController = () => {
+    if (isRecording) {
+      microphone.start(); 
+      microphone.onend = () => {
+        console.log("continue.."); 
+      };
+    } else {
+      microphone.stop(); 
+      microphone.onend = () => {
+        console.log("Stopped microphone on Click"); 
+      };
+    }
+    microphone.onstart = () => {
+      console.log("microphones on");
+    };
+  
+    microphone.onresult = (event) => {
+      const recordingResult = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join("");
+      console.log(recordingResult);
+      setNote(recordingResult);
+      microphone.onerror = (event) => {
+        console.log(event.error);
+      };
+    };
+  };  
+
+  const storeNote = () => {
+    setnotesStore([...notesStore, note]);
+    setNote("");
+  }; 
+
+  useEffect(() => {
+    startRecordController();
+  }, [isRecording]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Record Voice Notes</h1>
+      <div>
+        <div className="noteContainer">
+          <h2>Record Note Here</h2>
+          {isRecording ? <span>Recording... </span> : <span>Stopped </span>}
+          <button className="button" onClick={storeNote} disabled={!note}>
+            Save
+          </button>
+          <button onClick={() => setisRecording((prevState) => !prevState)}>
+            Start/Stop
+          </button>
+          <p>{note}</p>
+        </div>
+        <div className="noteContainer">
+          <h2>Notes Store</h2>
+          **{notesStore.map((note) => (
+            <p key={note}>{note}</p>
+          ))}**
+        </div>
+      </div>
+    </>
   );
 }
 
--- notes.md
+++ notes.md
@@ -2,3 +2,6 @@ Creating a simple todo list with speach to text functionalitity
 install a ruby on rails api with a postgresql database 
 run bundle install
 run rails db:create 
+create client folder
+run npx create-react-app . inside client folder 
+push everything to github 
