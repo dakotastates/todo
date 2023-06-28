de9751299685e214f40390b40574b0f9c673907f
dakotastates <59000108+dakotastates@users.noreply.github.com>
2023-06-27 19:44:08 -0700

    contact profile page design

--- client/src/pages/ContactProfilePage.js
+++ client/src/pages/ContactProfilePage.js
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { ArrowLeft } from 'react-bootstrap-icons';
+import { ArrowLeft, ThreeDotsVertical, Star, Envelope, Telephone, Balloon, Plus  } from 'react-bootstrap-icons';
 import '../components/Contacts/ContactsProfile.css'
 
 const ContactProfilePage = () =>{
@@ -7,15 +7,27 @@ const ContactProfilePage = () =>{
     const navigate = useNavigate();
 
     return(
-        <div className='contacts__profile-container'>
-            <div className='contacts__profile-header'>
-                <div onClick={()=> navigate(-1)}><ArrowLeft /></div>
-                <div>image</div>
-                <div>name and label</div>
-                <div>Edit Button</div>
+        <div className='profile__container'>
+            <div className='profile__header'>
+                <div className='profile__back-btn' onClick={()=> navigate(-1)}><ArrowLeft /></div>
+                <img className='profile__avatar' src='https://www.pngarts.com/files/10/Default-Profile-Picture-PNG-Image-Transparent-Background.png' />
+                <div className='profile__name-container'>
+                    <div className='profile__name'>Contact 1</div>
+                    <div className='profile__label-btn'><Plus /> Label</div>
+                </div>
+                <div className='profile__menu'>
+                    <Star />
+                    <ThreeDotsVertical />
+                    <div className='profile__edit-btn'>Edit</div>
+                </div>
             </div>
-            <div>nav bar</div>
-            <div>contact details</div>
+            <div className='profile__divider' />
+            <div className='profile__details-container'>
+                <div className='profile__details-label'>Contact Details</div>
+                <div><Envelope /> Add email</div>
+                <div><Telephone /> Add phone </div>
+                <div><Balloon /> Add birthday</div>
+            </div>
         </div>
     )
 
